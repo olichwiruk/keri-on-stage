114d7bc72c1844ca9b55d9d59ae2ead9119b9fc1
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2025-01-21 17:12:38 +0100

    feat: add hello world with ractor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use ractor::{Actor, ActorRef, ActorProcessingErr};
+
+#[tokio::main]
+async fn main() {
+    let (actor, actor_handle) = Actor::spawn(None, MyFirstActor, ()).await.expect("Actor failed to start");
+    
+    for _i in 0..10 {
+        actor.cast(MyFirstActorMessage::PrintHelloWorld).expect("Failed to send message to actor");
+    }
+
+    tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+
+    actor.stop(None);
+    actor_handle.await.unwrap();
+}
+
+pub struct MyFirstActor;
+
+pub enum MyFirstActorMessage {
+    PrintHelloWorld,
+}
+
+impl Actor for MyFirstActor {
+    type State = ();
+    type Msg = MyFirstActorMessage;
+    type Arguments = ();
+
+    async fn pre_start(&self, _myself: ActorRef<Self::Msg>, _arguments: Self::Arguments)
+        -> Result<Self::State, ActorProcessingErr>
+    {
+        Ok(())
+    }
+
+    async fn handle(&self, _myself: ActorRef<Self::Msg>, message: Self::Msg, _state: &mut Self::State) 
+        -> Result<(), ActorProcessingErr>
+    {
+        match message {
+            MyFirstActorMessage::PrintHelloWorld => {
+                println!("Hello world!");
+            }
+        }
+        Ok(())
+    }
 }
